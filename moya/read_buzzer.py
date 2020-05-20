5866c8ebac15004a8be06d1d3adf38ed9a515172
duru <enfn2001@gmail.com>
2020-05-20 17:25:14 +0900

    rfid 쓰기 기능 구현

--- moya/read_buzzer.py
+++ moya/read_buzzer.py
@@ -4,9 +4,27 @@ import platform
 def rfid_read():
         return "rfid read--->"
 
+def rfid_write():
+        sysnm = platform.system()
+        if sysnm is "Linux":
+                import RPi.GPIO as GPIO
+                from mfrc522 import SimpleMFRC522
+                reader = SimpleMFRC522()
+
+                try:
+                        text = "test1"
+                        print("Now place your tag to write")
+                        reader.write(text)
+                except Exception as e:
+                         print("write error  %d: %s" %(e.args[0], e.args[1]))
+                         raise
+                finally:
+                        GPIO.cleanup()
+                        return "write"
+
 def rfid_rpi_read():
         sysnm = platform.system()
-        if sysnm is "rpi":
+        if sysnm is "Linux":
                 import RPi.GPIO as GPIO
                 from mfrc522 import SimpleMFRC522
                 from time import sleep
