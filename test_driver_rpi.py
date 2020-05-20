21aea2aef9f8d56800e427c21ca2accd5ef93cdd
duru <enfn2001@gmail.com>
2020-05-20 21:31:05 +0900

    rpi buzz tester with driver

---
+++ test_driver_rpi.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from moya.driver_rpi import  rfid_write, rfid_read
+
+
+print("rfid write test")
+print(rfid_write())
+print("rfid write test-----")
+
+
+print("rpi buzz test")
+print(rfid_read())
+print("rfid buzz test-----")
