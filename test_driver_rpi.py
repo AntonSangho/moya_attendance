931047a2c4438483de4f1760cad661504b502770
duru <enfn2001@gmail.com>
2020-05-20 22:17:17 +0900

    rfid write test3

--- test_driver_rpi.py
+++ test_driver_rpi.py
@@ -8,7 +8,8 @@ hash = random.getrandbits(128)
 
 print("hash value: %032x" % hash)
 print("rfid write test")
-print(rfid_write(hashlib.md5("{hash}".encode("utf8"))))
+tag = input("input tag")
+print(rfid_write(tag))
 print("rfid write test-----")
 
 
