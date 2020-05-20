1e94039d64dfa70661067c22308cf9ec438b7c66
duru <enfn2001@gmail.com>
2020-05-20 17:15:50 +0900

    라스베리파이 플래폼 확인 문자열

--- moya/Write.py
+++ moya/Write.py
@@ -2,9 +2,10 @@
 
 # import RPi.GPIO as GPIO
 # from mfrc522 import SimpleMFRC522
+import platform
 
-# import platform
-
+#라스베리파이에서 나오는 문자열 확인
+print(platform.system())
 # reader = SimpleMFRC522()
 
 # try:
