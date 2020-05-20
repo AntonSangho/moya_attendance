771cd7ef7283bd1dd5ee5f3b6c9ad961d838317a
duru <enfn2001@gmail.com>
2020-05-20 21:56:09 +0900

    rfid read buzz test2

--- moya/read.py
+++ moya/read.py
@@ -6,11 +6,10 @@ import RPi.GPIO as GPIO
 
 def read():
     try:
-        while True:
-            print("Hold a tag near the reader")
-            id, text = reader.read()
-            print("ID: %s\nText: %s" % (id,text))
-            sleep(1)
+        print("Hold a tag near the reader")
+        id, text = reader.read()
+        print("ID: %s\nText: %s" % (id,text))
+        sleep(1)
     except KeyboardInterrupt:
         GPIO.cleanup()
         raise
