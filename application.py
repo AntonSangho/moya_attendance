8851042dfb0e5b7b342c21efb2eb28c5f54902cf
duru <enfn2001@gmail.com>
2020-05-25 17:06:08 +0900

    입력 오류 수정

--- application.py
+++ application.py
@@ -29,7 +29,7 @@ def entry(user=''):
 @application.route('/exit/<user>')
 def entry(user=''):
     print(application.env)
-    return render_template('entry.html', msg="카드를 올려 놓으세요!", platform="입장")
+    return render_template('entry.html', msg="카드를 올려 놓으세요!", platform="퇴장")
     # url test1 request path /두루
     # url test2 request path /
     # if user exist turn on green led
@@ -42,7 +42,7 @@ def endpoint_rfid_read():
     print("rfid buzz test-----")
     if rst[0] != "not support this platform.":
         print(f"{rst[1]}, {rst[2]}")
-        userid, username = int(rst[2].split('^')
+        userid = int(rst[2])
         db = init_connect_db()
         rst.append("DB TRUE" if set_attendance(db, userid) else "DB FALSE")
 
