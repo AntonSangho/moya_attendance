1ec9e1e68b254d80c9f2915462df2e10f288a5af
duru <enfn2001@gmail.com>
2020-05-25 18:28:27 +0900

    GIPO 실행시 무존건 초기화

--- moya/read.py
+++ moya/read.py
@@ -2,11 +2,11 @@ from time import sleep
 import sys
 from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
+reader = SimpleMFRC522()
 
 def read():
     try:
-        reader = SimpleMFRC522()
-        
+        GPIO.cleanup()       
         print("Hold a tag near the reader")
         id, text = reader.read()
         print("ID: %s\nText: %s" % (id,text))
