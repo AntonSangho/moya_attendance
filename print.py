d79aa8d4344cb725c8e0062be9ac02ae2be28cab
anton <sanghoemail@gmail.com>
2020-07-14 15:12:31 +0900

    add print.py

--- print.py
+++ print.py
@@ -1,4 +1,9 @@
-#!/bin/python
+"""
+Printe on demand with rasberrypi (https://www.hackster.io/glowascii/print-on-demand-with-raspi-d74619)
+GPIO pin:
+
+"""
+# !/bin/python
 
 import RPi.GPIO as GPIO
 import time
@@ -6,4 +11,14 @@ import os
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-os.system("")
+
+
+def Printtest(channel):
+    print('Printing...')
+
+    # Test printing
+    os.system("echo 'This is a test.' | lp")
+
+
+while 1:
+    time.sleep(1)
