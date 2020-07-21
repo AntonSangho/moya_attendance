fca61ba74636d1f137704e1df726947ac2a8f58f
AntonSangho <sanghoemail@gmail.com>
2020-07-21 17:08:10 +0100

    change to random print

--- print.py
+++ print.py
@@ -14,19 +14,27 @@ import RPi.GPIO as GPIO
 import time
 import os
 import cups
+import random 
 
 conn = cups.Connection()
 printers = conn.getPrinters()
 printer_name = printers.keys()[0]
-file = "/home/pi/moya_attendance/image/w1.png"
+file1 = "/home/pi/moya_attendance/image/w1.png"
+file2 = "/home/pi/moya_attendance/image/w2.png"
+file3 = "/home/pi/moya_attendance/image/w3.png"
+file4 = "/home/pi/moya_attendance/image/w4.png"
+file5 = "/home/pi/moya_attendance/image/w5.png"
+filelist = [file1, file2, file3, file4, file5]
+
 #conn.printFile(printer_name, file, "W1.png", {})
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 def Printtest(channel):
     print('Printing...')
-    conn.printFile(printer_name, file, "W1.png", {})
+    conn.printFile(printer_name, random.choice(filelist), "working diary", {})
 
 
 # Test printing
