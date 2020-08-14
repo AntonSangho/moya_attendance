979594b0fced971b10bbd7589a22684ff3385919
AntonSangho <sanghoemail@gmail.com>
2020-08-14 14:18:04 +0100

    add rfid function

--- application.py
+++ application.py
@@ -248,12 +248,13 @@ def endpoint_rfid_read_entry():
 def endpoint_rfid_read():
     try:
         print("rpi buzz")
-
+        rfid_uid=""
         rst = rfid_read()
         if rst[0] != "not support the platform.":
             db = init_connect_db()
             if rst[1] != None:
                 rfid_uid = rst[1]
+                print('%s-----' % rfid_uid)
     except Exception as e:
         print("error", e)
         return abort(500)
