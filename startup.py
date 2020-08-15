662e85975210f1b35bbbeea8182e736a20cd4c43
anton <sanghoemail@gmail.com>
2020-08-15 17:30:27 +0900

    shutdown 기능제거

--- startup.py
+++ startup.py
@@ -3,23 +3,15 @@ from threading import Thread
 import os
 
 
-def startprgm(i):
-    print("Running thread %d" % i)
-    if i == 0:
-        time.sleep(1)
-        print('Running: shutdown command')
-        os.system("sudo shutdown -h now")
-    elif i == 1:
-        print('Running: gpio.py')
-        time.sleep(1)
-        os.system("sudo modeprobe -i uinput")
-        time.sleep(1)
-        os.system("sudo python3 /home/pi/moya_attendance/gpio.py")
-        print('Running: application.py')
-        time.sleep(1)
-        os.system("sudo python3 /home/pi/moya_attendance/application.py")
-    else:
-        pass
+def startprgm():
+    print('Running: gpio.py')
+    time.sleep(1)
+    os.system("sudo modeprobe -i uinput")
+    time.sleep(1)
+    os.system("sudo python3 /home/pi/moya_attendance/gpio.py")
+    print('Running: application.py')
+    time.sleep(1)
+    os.system("sudo python3 /home/pi/moya_attendance/application.py")
 
 
 for i in range(2):
