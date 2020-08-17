9bd78c19fb485c1cc1dfe583470f88b9d84f15d1
AntonSangho <sanghoemail@gmail.com>
2020-08-17 04:02:05 +0100

    새로운카드 등록할때 매시지 나오는 부분 수정

--- test_driver_rpi.py
+++ test_driver_rpi.py
@@ -3,15 +3,16 @@ from moya.driver_rpi import  rfid_write, rfid_read
 
 import hashlib 
 import random
-
+import RPi.GPIO as GPIO
 hash = random.getrandbits(128)
 
 #print("hash value: %032x" % hash)
 #print("rfid write test")
-tag = input("input tag")
-print(rfid_write(tag))
-print("rfid write test-----")
+#tag = input("input tag")
+#print(rfid_write(tag))
+#print("rfid write test-----")
 
+GPIO.cleanup()
 
 print("rpi buzz test")
 print(rfid_read())
