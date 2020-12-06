1661d88c349ecc66f568ab580d89129ad5a1067a
anton <sanghoemail@gmail.com>
2020-12-06 21:48:07 +0900

    사용하지 않는 add_newcard_mh제거

--- application_mh.py
+++ application_mh.py
@@ -17,7 +17,7 @@ from datetime import date
 
 from moya.driver_rpi import rfid_read, rfid_write, buzzer_call
 from moya.driver_db import init_connect_db, get_attendance, set_exit_mh, get_userinfo, get_userlist, \
-    set_signup, is_rfid_mh, add_newcard_mh, get_rfid, get_dayattendance, get_RangeAttendance, get_userdetail, \
+    set_signup, is_rfid_mh, get_rfid, get_dayattendance, get_RangeAttendance, get_userdetail, \
     get_userattendance, set_modify, get_userselectdetail, get_adduserlist, set_attendance_mh
 from sqlalchemy import create_engine
 
