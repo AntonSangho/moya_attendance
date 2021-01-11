fde2ffc6507c51d6a7b0594cd2012094d3412691
anton <sanghoemail@gmail.com>
2021-01-11 12:54:35 +0900

    localhost가 http://에서 실행되도록 수정

--- kiosk.sh
+++ kiosk.sh
@@ -8,7 +8,7 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://localhost:5000/webapp &
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:5000/webapp &
 
 while true; do
    xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
