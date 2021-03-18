f717d4db3ff528254bedb664a372d37b0adcf8a3
anton <sanghoemail@gmail.com>
2021-03-18 23:34:09 +0900

    제천도서관 회원명부 한글깨짐해결

--- application.py
+++ application.py
@@ -306,8 +306,16 @@ def userlist():
     print(request.method)
     if request.method == 'POST':
         df = pd.DataFrame(get_userdetail(db))
-        csv_data = df.to_csv(index='false', encoding='utf-8')
-        response = Response(csv_data, mimetype='text/csv')
+        output = StringIO()
+        output.write(u'\ufeff') # 한글인코딩을 위해 UTF-8 with BOM 설정해주기 
+        df.to_csv(output) # CSV 파일 형태로 브라우저가 파일 다운로라고 인식하도록 만들어주기 
+        response = Response(
+            output.getvalue(),
+            mimetype="text/csv",
+            content_type='application/octet-strem',
+        )
+        #csv_data = df.to_csv(index='false', encoding='utf-8')
+        #response = Response(csv_data, mimetype='text/csv')
         response.headers.set("Content-Disposition", "attachment", filename="userlist.csv")
         return response
     if 'reliquum' in session:
