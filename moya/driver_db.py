864024020b7bba3284b7826ba22635f5a556bf48
anton <sanghoemail@gmail.com>
2021-03-23 14:14:24 +0900

    개발용 회원정보 수정하기 추가

--- moya/driver_db.py
+++ moya/driver_db.py
@@ -626,6 +626,24 @@ def set_modify_sw(db, selected_name, sex, year, phone, memo):
         db.close()
         return 1
 
+def set_modify_test(db, selected_name, sex, year, phone, memo):
+    try:
+        cursor = db.cursor()
+        # cursor.execute(f"{sqlmapper['sql_9_admin2']}",({sex}, {year}, {phone}, {memo}, {selected_name}))
+        cursor.execute(
+            f"UPDATE dev_users_detail SET sex='{sex}', year={year}, phone='{phone}', memo='{memo}' where name ='{selected_name}' ;")
+        print("1_1 - set_modify try")
+    except pymysql.Error as e:
+        db.rollback()
+        db.close()
+        # print("1_2 - set_modify exception")
+        print("db error pymysql %d: %s" % (e.args[0], e.args[1]))
+        return 0
+    else:
+        db.commit()
+        db.close()
+        return 1
+
 
 def set_signup(db, id, rfid, name, sex, year, phone, memo):
     try:
