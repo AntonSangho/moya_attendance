c7324f2fdd29e44d2f6d99cb08efe49c250e7b15
AntonSangho <sanghoemail@gmail.com>
2021-11-09 17:44:04 +0900

    세종 자료받는페이지 접속오류 해결

--- application.py
+++ application.py
@@ -1451,8 +1451,10 @@ def daterange_sj():
     if request.method == 'POST':
         StartDate = form.dStart.data.strftime('%Y-%m-%d')
         EndDate = form.dEnd.data.strftime('%Y-%m-%d')
+        db = get_conn()
         df = pd.DataFrame(get_RangeAttendance_sj(db, StartDate, EndDate))
         output = StringIO()
+        
         output.write(u'\ufeff') # 한글인코딩을 위해 UTF-8 with BOM 설정해주기 
         df.to_csv(output) # CSV 파일 형태로 브라우저가 파일 다운로라고 인식하도록 만들어주기 
         response = Response(
