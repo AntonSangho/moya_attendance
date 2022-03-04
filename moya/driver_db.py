2325b6cade3d0a7693c9e037d5cb799be0c9d033
AntonSangho <sanghoemail@gmail.com>
2022-03-04 16:31:13 +0900

    도서관별 get_existence 추가

--- moya/driver_db.py
+++ moya/driver_db.py
@@ -858,6 +858,66 @@ def get_existence_test(db, userid):
         print("db error pymysql %d: %s" % (e.args[0], e.args[1]))
         return 0 
 
+# [제천] 작은손 기록여부 확인 (1: 출입기록이 없는 작은손, 0: 출입기록이 있는 작은손)  
+def get_existence(db, userid):
+    try:
+        cursor = db.cursor()
+        cursor.execute(f"""
+        select not exists (select * from stat_attendance where userid ='{userid}') as value; 
+        """)
+        return cursor.fetchone()
+    except pymysql.error as e:
+        print("db error pymysql %d: %s" % (e.args[0], e.args[1]))
+        return 0 
+
+# [마하] 작은손 기록여부 확인 (1: 출입기록이 없는 작은손, 0: 출입기록이 있는 작은손)  
+def get_existence_mh(db, userid):
+    try:
+        cursor = db.cursor()
+        cursor.execute(f"""
+        select not exists (select * from mh_stat_attendance where userid ='{userid}') as value; 
+        """)
+        return cursor.fetchone()
+    except pymysql.error as e:
+        print("db error pymysql %d: %s" % (e.args[0], e.args[1]))
+        return 0 
+
+# [수원] 작은손 기록여부 확인 (1: 출입기록이 없는 작은손, 0: 출입기록이 있는 작은손)  
+def get_existence_sw(db, userid):
+    try:
+        cursor = db.cursor()
+        cursor.execute(f"""
+        select not exists (select * from sw_stat_attendance where userid ='{userid}') as value; 
+        """)
+        return cursor.fetchone()
+    except pymysql.error as e:
+        print("db error pymysql %d: %s" % (e.args[0], e.args[1]))
+        return 0
+
+# [반포] 작은손 기록여부 확인 (1: 출입기록이 없는 작은손, 0: 출입기록이 있는 작은손)  
+def get_existence_bp(db, userid):
+    try:
+        cursor = db.cursor()
+        cursor.execute(f"""
+        select not exists (select * from bp_stat_attendance where userid ='{userid}') as value; 
+        """)
+        return cursor.fetchone()
+    except pymysql.error as e:
+        print("db error pymysql %d: %s" % (e.args[0], e.args[1]))
+        return 0
+
+# [세종] 작은손 기록여부 확인 (1: 출입기록이 없는 작은손, 0: 출입기록이 있는 작은손)  
+def get_existence_sj(db, userid):
+    try:
+        cursor = db.cursor()
+        cursor.execute(f"""
+        select not exists (select * from sj_stat_attendance where userid ='{userid}') as value; 
+        """)
+        return cursor.fetchone()
+    except pymysql.error as e:
+        print("db error pymysql %d: %s" % (e.args[0], e.args[1]))
+        return 0
+
 #[세종] 작은손의 최대 작업시간, 방문시간, 지금까지 작업시간을 카드id로 확인하기
 def get_workingtimeWithUserid_sj(db, userid):
     try:
